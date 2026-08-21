9697121598ec6fcf525079c6cc8f2280a1e668aa
山泥若叶睦 <102826168@qq.com>
2026-08-21 23:55:48 +0800

    修复 Litestar 测试文件中的导入格式

--- tests/ashka/integrations/test_litestar.py
+++ tests/ashka/integrations/test_litestar.py
@@ -1,7 +1,7 @@
 from subprocess import run
 from sys import executable
 
-from ashka.integrations.litestar import get_container,setup_dishka
+from ashka.integrations.litestar import get_container, setup_dishka
 
 from dishka import AsyncContainer, make_async_container
 from litestar import Litestar
