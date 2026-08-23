c2e16a36d9ed470b16a32526e5166906d1efb459
山泥若叶睦 <102826168@qq.com>
2026-08-24 06:00:07 +0800

    Feature：假如上游将 container 转换为可弱引用对象，则自动使用 WeakKeyDictionary。

--- src/ashka/entities/bootstrap.py
+++ src/ashka/entities/bootstrap.py
@@ -1,11 +1,23 @@
-from weakref import WeakSet
+from weakref import WeakKeyDictionary, WeakSet
 
 from dishka import AsyncContainer, Container, DependencyKey
 from dishka.provider.make_factory import ProvideSource
 
 bootstrap_sources: WeakSet[ProvideSource] = WeakSet()  # pyright: ignore[reportUnknownVariableType]
 
-bootstrap_keys_by_container: dict[
-    Container | AsyncContainer,
-    list[DependencyKey],
-] = {}
+if all(
+    hasattr(Container_, "__weakref__") for Container_ in (Container, AsyncContainer)
+):
+    bootstrap_keys_by_container: (  # pragma: nocover
+        WeakKeyDictionary[
+            Container | AsyncContainer,
+            list[DependencyKey],
+        ]
+        | dict[
+            Container | AsyncContainer,
+            list[DependencyKey],
+        ]
+    ) = WeakKeyDictionary()
+
+else:
+    bootstrap_keys_by_container = {}  # pragma: nocover
