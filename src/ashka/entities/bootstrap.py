1b1953701039d84ec497b96e984f7a61b79d63f4
山泥若叶睦 <102826168@qq.com>
2026-08-24 04:43:15 +0800

    docs: update bootstrap_sources to use WeakSet for better memory management

--- src/ashka/entities/bootstrap.py
+++ src/ashka/entities/bootstrap.py
@@ -1,7 +1,9 @@
+from weakref import WeakSet
+
 from dishka import AsyncContainer, Container, DependencyKey
 from dishka.provider.make_factory import ProvideSource
 
-bootstrap_sources: set[ProvideSource] = set()  # pyright: ignore[reportUnknownVariableType]
+bootstrap_sources: WeakSet[ProvideSource] = WeakSet()  # pyright: ignore[reportUnknownVariableType]
 
 bootstrap_keys_by_container: dict[
     Container | AsyncContainer,
